5eb7b59b52368523bb6dbc3681f5506cbe470c99
DANIEL WILLIANS IGNACIO DE SOUZA <daniel.souza54@fatec.sp.gov.br>
2018-09-19 12:57:19 -0300

    Adicionando atividade 1a

---
+++ Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Circulo.java
@@ -0,0 +1,7 @@
+package br.gov.fatec.calculando;
+
+public class Circulo {	
+	private double raio;
+	private double raiz;
+	
+}
---
+++ Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Losango.java
@@ -0,0 +1,5 @@
+package br.gov.fatec.calculando;
+
+public class Losango {
+
+}
