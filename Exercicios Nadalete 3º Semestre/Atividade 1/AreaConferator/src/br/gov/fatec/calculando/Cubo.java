1fba4a93c8722f801e5467a27feab8c4452bcc03
DANIEL WILLIANS IGNACIO DE SOUZA <daniel.souza54@fatec.sp.gov.br>
2018-09-19 20:19:06 -0300

    Alterando contas

--- Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Cubo.java
+++ Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Cubo.java
@@ -1,5 +1,6 @@
 package br.gov.fatec.calculando;
 
 public class Cubo {
-
+	private Double lado;
+	private Double altura;
 }
--- Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Hexagono.java
+++ Exercicios Nadalete 3º Semestre/Atividade 1/AreaConferator/src/br/gov/fatec/calculando/Hexagono.java
@@ -1,5 +1,10 @@
 package br.gov.fatec.calculando;
 
 public class Hexagono {
-
+	private Double lados1;
+	private Double lados2;
+	private Double lados3;
+	private Double lados4;
+	private Double lados5;
+	private Double lados6;
 }
