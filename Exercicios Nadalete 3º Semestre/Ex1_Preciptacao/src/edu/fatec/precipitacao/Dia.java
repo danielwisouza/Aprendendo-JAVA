7e511092145c14398f1e5f17d99d2ea50051a3dd
DANIEL WILLIANS IGNACIO DE SOUZA <daniel.souza54@fatec.sp.gov.br>
2018-10-28 00:12:56 -0300

    Organização de Classes - Ex1-Preciptacao

---
+++ Exercicios Nadalete 3º Semestre/Ex1_Preciptacao/src/edu/fatec/precipitacao/Dia.java
@@ -0,0 +1,10 @@
+package edu.fatec.precipitacao;
+
+import java.time.LocalDate;
+
+import edu.fatec.classes.Dia;
+
+//public class Dia implements Comparable<Dia>{
+	private LocalDate dia;
+	private double totalChuva;
+}
