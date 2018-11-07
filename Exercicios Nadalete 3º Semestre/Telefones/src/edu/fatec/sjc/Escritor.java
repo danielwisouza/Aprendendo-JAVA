7bc33ea2666890c0ca949d6e6e48ab8eed068316
DANIEL WILLIANS IGNACIO DE SOUZA <daniel.souza54@fatec.sp.gov.br>
2018-11-07 20:22:45 -0200

    Teste leitura de arquivo

--- Exercicios Nadalete 3º Semestre/Telefones/src/edu/fatec/sjc/Escritor.java
+++ Exercicios Nadalete 3º Semestre/Telefones/src/edu/fatec/sjc/Escritor.java
@@ -1,5 +1,19 @@
 package edu.fatec.sjc;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Escritor {
-
+ 
+  public static void main(String args[]) throws IOException) {
+	  FileReader f = new FileReader("Endeço");
+	  BufferedReader in = new BufferedReader(f);
+	  String linha = in.readLine();
+	  while (linha != null){
+		System.out.println(linha);
+		linha = in.readLine();
+	  }
+	  in.close();
 }
+
